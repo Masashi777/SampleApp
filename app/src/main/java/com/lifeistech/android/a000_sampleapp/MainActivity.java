6bc54a9f8389bde1f9decba81450214d7353d579
Masashi <hikozaru.kinopio@gmail.com>
2017-01-17 19:09:50 +0900

    second

--- app/src/main/java/com/lifeistech/android/a000_sampleapp/MainActivity.java
+++ app/src/main/java/com/lifeistech/android/a000_sampleapp/MainActivity.java
@@ -5,6 +5,8 @@ import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
+    int number;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
