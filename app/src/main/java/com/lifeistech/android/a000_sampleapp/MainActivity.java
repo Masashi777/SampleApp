9e1eee32cb542d41bf743d698e0757c930ad80df
Masashi <hikozaru.kinopio@gmail.com>
2017-01-23 21:16:00 +0900

    add+

--- app/src/main/java/com/lifeistech/android/a000_sampleapp/MainActivity.java
+++ app/src/main/java/com/lifeistech/android/a000_sampleapp/MainActivity.java
@@ -8,7 +8,7 @@ import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
-    int number;
+    int number = 0;
 
     TextView textView;
 
@@ -28,6 +28,9 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        textView = (TextView)findViewById(R.id.textView);
+        textView.setText(String.valueOf(number));
     }
 
     public void add(View v) {
